fa06f2880970e3ae4633aec492a62efe320d4648
vimrzhang <vimrzhang@tencent.com>
2020-05-14 20:49:02 +0800

    version 1

---
+++ VFramework/Assets/Editor/GeneratorResConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class GeneratorResConfig : Editor
+{
+    /// <summary>
+    /// 将Resources里的Prefab名字和完整路径存储起来
+    /// </summary>
+    [MenuItem("Tools/Resources/GeneratorResConfig")]
+    public static void GeneratorRes()
+    {
+        string[] assets = AssetDatabase.FindAssets("t:prefab", new string[] { "Assets/Resources" });
+        for(int i = 0; i < assets.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(assets[i]);
+
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string filePath = path.Replace("Assets/Resources/", string.Empty).Replace(".prefab", string.Empty);
+            assets[i] = fileName + "=" + filePath;
+        }
+
+        //写入文件
+        File.WriteAllLines("Assets/StreamingAssets/ResConfig.txt", assets);
+        
+        AssetDatabase.Refresh();
+    }
+}
---
+++ VFramework/Assets/Scripts/Common/ResourcesMgr.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ResourcesMgr
+{
+    private static Dictionary<string, string> m_resConfigDic;
+
+    static ResourcesMgr()
+    {
+        //加载文件
+        string fileContent = GetConfigFile();
+        //解析文件
+        BuildMap(fileContent);
+    }
+
+    /// <summary>
+    /// 加载文件
+    /// </summary>
+    /// <returns></returns>
+    private static string GetConfigFile()
+    {
+        string url;
+#if UNITY_EDITOR || UNITY_STANDALONE
+        url = "file://" + Application.dataPath + "/StreamingAssets/ResConfig.txt";
+#elif UNITY_ANDROID
+        url = "jar:file://" + Application.dataPath + "!/assets/ResConfig.txt";
+#elif UNITY_IPHONE
+        url = "file://" + Application.dataPath + "/Raw/ResConfig.txt";
+#endif
+        UnityWebRequest webRequest = UnityWebRequest.Get(url);
+        webRequest.SendWebRequest();
+        while (true)
+        {
+            if (webRequest.isDone)
+            {
+                return webRequest.downloadHandler.text; 
+            }
+        }
+    }
+
+    private static void BuildMap(string fileContent)
+    {
+        m_resConfigDic = new Dictionary<string, string>();
+
+        //当程序退出using代码块，将自动调用reader.Dispose方法
+        using (StringReader reader = new StringReader(fileContent))
+        {
+            string line;
+            while((line = reader.ReadLine())!=null)
+            {
+                string[] keyValue = line.Split('=');
+                m_resConfigDic.Add(keyValue[0], keyValue[1]);
+            }
+        }
+    }
+
+    public static T Load<T>(string fileName) where T : Object
+    {
+        string prefabPath = m_resConfigDic[fileName];
+        return Resources.Load<T>(prefabPath);
+    }
+
+}
